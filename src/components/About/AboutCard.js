d683f972472df8cd0d45e020295a99ab0acca618
VikasKumar281 <179719586+VikasKumar281@users.noreply.github.com>
2026-09-18 18:13:03 +0530

    Updated About & Toolstack Section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,34 +8,52 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Vikas Kumar</span> from{" "}
+            Hi everyone! I’m{" "}
+            <span className="purple">Vikas Kumar</span> from{" "}
             <span className="purple">Kanpur, Uttar Pradesh, India</span>.
             <br />
-            I am currently pursuing{" "}
-            <span className="purple">B.Tech in Mechatronics Engineering</span> at{" "}
+
+            I recently completed my{" "}
+            <span className="purple">
+              B.Tech in Mechatronics Engineering
+            </span>{" "}
+            from{" "}
             <span className="red">
               Indian Institute of Information Technology Bhagalpur
             </span>
             .
             <br />
             <br />
+
             I am passionate about{" "}
-            <span className="purple">Web Development</span> and enjoy building
-            responsive and user-friendly web applications using modern
-            technologies.
+            <span className="purple">Full Stack Web Development</span> and
+            enjoy building responsive, user-friendly, and scalable web
+            applications using modern technologies.
             <br />
             <br />
+
+            I have gained practical industry experience as a{" "}
+            <span className="purple">
+              Full Stack Developer Intern at CyberWarFare Labs
+            </span>
+            , where I worked on real-world web applications, backend REST APIs,
+            UI improvements, API testing, and development environments using
+            modern tools and technologies.
+            <br />
+            <br />
+
             I have hands-on experience with{" "}
             <span className="purple">
-              React.js, Node.js, Express.js, MongoDB, HTML, CSS, and Tailwind CSS
-            </span>{" "}
-            and I actively practice{" "}
-            <span className="purple">
-              Data Structures & Algorithms 
+              React.js, JavaScript, Node.js, Express.js, MongoDB, REST APIs,
+              HTML, CSS, and Tailwind CSS
             </span>
-             {" "} and {" "} 
+            . I also have practical experience with{" "}
             <span className="purple">
-               Problem Solving
+              Git, Docker, Kubernetes, Linux, and Postman
+            </span>{" "}
+            and actively practice{" "}
+            <span className="purple">
+              Data Structures & Algorithms and Problem Solving
             </span>
             .
           </p>
@@ -44,17 +62,20 @@ function AboutCard() {
             <li className="about-activity">
               <ImPointRight /> Solving DSA & Coding Problems 💡
             </li>
+
             <li className="about-activity">
-              <ImPointRight /> Building Web Projects 🚀
+              <ImPointRight /> Building Full Stack Web Projects 🚀
             </li>
+
             <li className="about-activity">
-              <ImPointRight /> Learning New Technologies 📚
+              <ImPointRight /> Learning & Exploring New Technologies 📚
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Learning every day and building things that create real impact."
           </p>
+
           <footer className="blockquote-footer">Vikas Kumar</footer>
         </blockquote>
       </Card.Body>
--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiNextdotjs } from "react-icons/si";
+import {
+  SiNextdotjs,
+  SiExpress,
+  SiKubernetes,
+  SiLinux,
+  SiNginx,
+  SiPostgresql,
+} from "react-icons/si";
+
 import C from "../../Assets/TechIcons/C++.svg";
 import Javascript from "../../Assets/TechIcons/Javascript.svg";
 import Node from "../../Assets/TechIcons/Node.svg";
@@ -19,67 +27,24 @@ import MUI from "../../Assets/TechIcons/MUI.svg";
 import Postman from "../../Assets/TechIcons/Postman.svg";
 import AWS from "../../Assets/TechIcons/AWS.svg";
 
-
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      
+      {/* Programming Languages */}
       <Col xs={4} md={2} className="tech-icons">
         <img src={C} alt="C++" />
         <div className="tech-icons-text">C++</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Javascript} alt="javascript" />
-        <div className="tech-icons-text">Javascript</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Typescript} alt="typescript" />
-        <div className="tech-icons-text">Typescript</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Node} alt="node" />
-        <div className="tech-icons-text">Node.Js</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={ReactIcon} alt="react" />
-        <div className="tech-icons-text">React.Js</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Mongo} alt="mongoDb" />
-        <div className="tech-icons-text">Mongo DB</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={SQL} alt="SQL" />
-        <div className="tech-icons-text">SQL</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Redux} alt="redux" />
-        <div className="tech-icons-text">Redux</div>
+        <img src={Javascript} alt="JavaScript" />
+        <div className="tech-icons-text">JavaScript</div>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs fontSize={"24px"} />
-        <div className="tech-icons-text">Next.js</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Git} alt="git" />
-        <div className="tech-icons-text">Git</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Firebase} alt="firebase" />
-        <div className="tech-icons-text">Firebase</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Redis} alt="redis" />
-        <div className="tech-icons-text">Redis</div>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={Docker} alt="docker" />
-        <div className="tech-icons-text">Docker</div>
-      </Col>
-
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={SQL} alt="SQL" />
-        <div className="tech-icons-text">Postgresql</div>
+        <img src={Typescript} alt="TypeScript" />
+        <div className="tech-icons-text">TypeScript</div>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
@@ -87,14 +52,99 @@ function Techstack() {
         <div className="tech-icons-text">Python</div>
       </Col>
 
+      {/* Frontend */}
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Tailwind} alt="tailwind" />
+        <img src={ReactIcon} alt="React.js" />
+        <div className="tech-icons-text">React.js</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Redux} alt="Redux" />
+        <div className="tech-icons-text">Redux</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs fontSize={"24px"} />
+        <div className="tech-icons-text">Next.js</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Tailwind} alt="Tailwind CSS" />
         <div className="tech-icons-text">Tailwind CSS</div>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <img src={MUI} alt="mui" />
+        <img src={MUI} alt="Material UI" />
         <div className="tech-icons-text">Material UI</div>
+      </Col>
+
+      {/* Backend */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Node} alt="Node.js" />
+        <div className="tech-icons-text">Node.js</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress fontSize={"24px"} />
+        <div className="tech-icons-text">Express.js</div>
+      </Col>
+
+      {/* Databases */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Mongo} alt="MongoDB" />
+        <div className="tech-icons-text">MongoDB</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql fontSize={"24px"} />
+        <div className="tech-icons-text">PostgreSQL</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={SQL} alt="SQL" />
+        <div className="tech-icons-text">SQL</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Redis} alt="Redis" />
+        <div className="tech-icons-text">Redis</div>
+      </Col>
+
+      {/* DevOps & Infrastructure */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Git} alt="Git" />
+        <div className="tech-icons-text">Git</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Docker} alt="Docker" />
+        <div className="tech-icons-text">Docker</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiKubernetes fontSize={"24px"} />
+        <div className="tech-icons-text">Kubernetes</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux fontSize={"24px"} />
+        <div className="tech-icons-text">Linux</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNginx fontSize={"24px"} />
+        <div className="tech-icons-text">Nginx</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={AWS} alt="AWS" className="tech-icon-images" />
+        <div className="tech-icons-text">AWS</div>
+      </Col>
+
+      {/* Tools & Services */}
+      <Col xs={4} md={2} className="tech-icons">
+        <img src={Firebase} alt="Firebase" />
+        <div className="tech-icons-text">Firebase</div>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
@@ -102,10 +152,6 @@ function Techstack() {
         <div className="tech-icons-text">Postman</div>
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons">
-        <img src={AWS} alt="Postman" className="tech-icon-images" />
-        <div className="tech-icons-text">AWS</div>
-      </Col>
     </Row>
   );
 }
